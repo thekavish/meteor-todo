9be4f23ae798d361e7ef728dfe245a1b64c44616
Kavish <kavish@technoscore.net>
2019-09-24 18:43:36 +0530

    basic component templating

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import Hello from './Hello.jsx'
-import Info from './Info.jsx'
+import React, { Component } from 'react'
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-)
+import Task from './comonents/Task'
+
+// App component - represents the whole app
+class App extends Component {
+  getTasks () {
+    return [
+      { _id: 1, text: 'This is task 1' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
+    ]
+  }
+
+  renderTasks () {
+    return this.getTasks().map((task) => (
+      <Task key={task._id} task={task}/>
+    ))
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default App
