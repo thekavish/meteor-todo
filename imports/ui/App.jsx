a2afaa1881b836df3b5b87254c8bef7873678358
Kavish <kavish@technoscore.net>
2019-09-25 12:07:35 +0530

    added form to insert data into db

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { withTracker } from 'meteor/react-meteor-data'
 
 import { Tasks } from '../api/models/tasks'
@@ -6,6 +7,21 @@ import Task from './components/Task'
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit (event) {
+    event.preventDefault()
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim()
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    })
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = ''
+  }
+
   renderTasks () {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task}/>
@@ -17,6 +33,10 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+          </form>
         </header>
 
         <ul>
@@ -29,6 +49,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   }
 })(App)
