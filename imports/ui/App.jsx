610ee677b4b96e5e279e144ce031b6671d1148f2
Kavish <kavish@technoscore.net>
2019-09-25 15:06:30 +0530

    added feature to filter out completed tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,14 @@ import Task from './components/Task'
 
 // App component - represents the whole app
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      hideCompleted: false,
+    }
+  }
+
   handleSubmit (event) {
     event.preventDefault()
 
@@ -22,8 +30,19 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = ''
   }
 
+  toggleHideCompleted () {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    })
+  }
+
   renderTasks () {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked)
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task}/>
     ))
   }
@@ -33,6 +52,15 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox" readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
